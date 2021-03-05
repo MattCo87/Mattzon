c8a294074fa2d6677e1c650a2d7950011683310b
Nicolas Vauché <hello@nicolasvauche.net>
2021-03-05 22:41:47 +0100

    Model Product: getProducts

--- php/Models/Product.php
+++ php/Models/Product.php
@@ -1,7 +1,15 @@
 <?php
 /**
+ * Récupération de tous les produits dans la base de données
+ */
+function getProducts($connexion) {
+    
+}
+
+
+/**
  * Récupération d'un produit dans la base de données
  */
 function getProduct($connexion, $id) {
-    
+
 }
